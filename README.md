10f95ceb23a5537be192585320ca07a867b3efa9
MaxSaveskul <maxsaveskul@gmail.com>
2022-06-23 22:08:25 +0300

    m

---
+++ README.md
@@ -0,0 +1,5 @@
+# My first try of project setup
+
+## How to run
+- `npm install`
+- `npm start`
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const a = 1;
+function foo(name) {
+    const lastName = name;
+    return lastName
+}
+const x = 200;
+foo('Max');
+
+console.log(x, a);
